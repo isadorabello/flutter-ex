a2340a423d0d7bbdb4670ccaa3f8ef9f9f9fc5f5
isadorabello <isadora.bello@gec.inatel.br>
2023-05-09 11:39:00 -0300

    feat: starting new login project

--- novo_projeto/lib/main.dart
+++ novo_projeto/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:novo_projeto/Login/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,11 +11,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
+      home: HomePage()
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: LoginScreen());
   }
 }
